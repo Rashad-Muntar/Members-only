54cdb51c5ed431e468e7e3f639026332728e6368
FdI96 <federicolopezce@gmail.com>
2021-04-02 11:50:05 -0300

    commit

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,8 @@ module PostsHelper
   def show_user_post(post)
     content_tag(:td, post.user_id) unless current_user.nil?
   end
+
+  def show_user_name
+    content_tag(:td, current_user.email) unless current_user.nil?
+  end
 end
