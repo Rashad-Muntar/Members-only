ae2dd6d5caf45942b6333b71073212886ebe53ec
Rashad-Muntar <muntartoure@gmail.com>
2021-04-03 16:57:44 +0000

    Add sign, sign out helper methods to views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+    def show_signout
+        out = ''
+        if user_signed_in?
+          out << link_to('Sign Out', destroy_user_session_path, method: :delete)
+        end
+        out.html_safe
+      end
+
+      def show_signin
+        out = ''
+        if !user_signed_in?
+          out << link_to('Sign in', user_session_path )
+        end
+        out.html_safe
+      end
 end
