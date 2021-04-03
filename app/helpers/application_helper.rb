00685d5a350a1ab0c9e0320b855c26db46087415
Rashad-Muntar <muntartoure@gmail.com>
2021-04-03 17:06:57 +0000

    Add sign-up helper method to views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-    def show_signout
+    def show_sign_out
         out = ''
         if user_signed_in?
           out << link_to('Sign Out', destroy_user_session_path, method: :delete)
@@ -8,10 +8,18 @@ module ApplicationHelper
         out.html_safe
       end
 
-      def show_signin
+      def show_sign_in
         out = ''
         if !user_signed_in?
           out << link_to('Sign in', user_session_path )
+        end
+        out.html_safe
+      end
+
+      def show_sign_up
+        out = ''
+        if !user_signed_in?
+          out << link_to('Sign up', new_user_registration_path)
         end
         out.html_safe
       end
