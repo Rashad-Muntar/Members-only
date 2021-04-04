0a60e86776829b30f59e8f996bfc6f5cd50427fd
Rashad-Muntar <muntartoure@gmail.com>
2021-04-04 13:25:06 +0000

    Add post error handling to post helper file

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,4 +1,6 @@
 module PostsHelper
+
+
   def show_user_post(post)
     content_tag(:td, post.user_id) unless current_user.nil?
   end
@@ -10,4 +12,14 @@ module PostsHelper
   def show_user_name
     content_tag(:td, current_user.username) unless current_user.nil?
   end
+
+  def display_error
+    content_tag(:ul) do
+      @post.errors.collect do |error|
+        content_tag(:li, error.full_message)
+      end.join.html_safe
+    end
+end
+
+
 end
